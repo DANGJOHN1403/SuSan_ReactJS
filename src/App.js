67d4f486fb13138c5b27de8b5e0c4371ce5036fb
DANGJOHN1403 <johndangdhdev@gmail.com>
2024-08-16 12:05:13 +0700

    comment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+// App.js
 import './App.css';
-
+import { Outlet } from 'react-router-dom';
+import Menu from './Menu';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="positon-relative d-flex" style={{ }}>
+        <div className="col-md-2 h-100 d-flex justify-content-center"> 
+            <div id="logo">SUSAN CÀ PHÊ</div>
+        </div>
+        <div className="col-md-10 position-relative">  
+          <Menu></Menu>
+        </div>
       </header>
+      <main className="bg-light" style={{ minHeight:'300px'}}>  
+         <Outlet/>
+      </main>
+      <footer className="">
+      </footer>
     </div>
   );
 }
-
 export default App;
+
---
+++ src/QuanTri/Loai_Them.js
@@ -0,0 +1,9 @@
+// src/QuanTri/Loai_Them.js
+const Loai_Them = () => {
+//các biến
+return (
+<div> 
+    <h1>Thêm loại</h1>  
+</div>
+)}
+export default Loai_Them;
